5f5cbe31db612b01bc8a2a56c32d1a5770a97b56
Avery Black <avery.c.black@gmail.com>
2021-12-08 21:31:30 -0800

    Day 9

---
+++ src/Solutions/9/Day 9.ts
@@ -0,0 +1,99 @@
+import * as chalk from "chalk";
+import { SolnExport } from "../../SolnExport";
+
+function checkVals (map: number[][], x: number, y:number, maxX: number, maxY :number) {
+    let val = map[y][x];
+    let adj = [];
+    if (x != 0) adj.push(map[y][x - 1]);
+    if (x != maxX - 1) adj.push(map[y][x + 1]);
+    if (y != 0) adj.push(map[y - 1][x]);
+    if (y != maxY - 1) adj.push(map[y + 1][x]);
+    let filt = adj.filter(a => val < a);
+    return filt.length == adj.length;
+}
+
+function soln1(input: string) : void {
+    let map = input.split("\n").map(row => row.split("").map(val => parseInt(val)));
+
+    let lastIdx = map.length;
+    let total = 0;
+    map.forEach((row, y) => {
+        row.forEach((val, x) => {
+            if (!checkVals(map, x, y, row.length, lastIdx)) return;
+            total += val + 1;
+        });
+    });
+
+    console.log(total);
+}
+
+interface Position {
+    x: number,
+    y: number,
+}
+
+function visitIncludes(visit: Position[], pos: Position) {
+    for (let visited of visit) {
+        if (visited.x == pos.x &&
+            visited.y == pos.y) return true;
+    }
+    return false;
+}
+
+function addPos (queue: Position[], x: number, y: number) {
+    let pos = {x: x, y: y};
+    queue.push(pos);
+}
+
+function checkBasin (map: number[][], x: number, y: number, maxX: number, maxY: number) : number {
+    let basin = [ ];
+    let queue = [ {x: x, y: y } ];
+    let visit = [];
+
+    while (queue.length != 0) {
+        let pos = queue.pop()!;
+        if (visitIncludes(visit, pos)) continue;
+        visit.push(pos);
+        if (map[pos.y][pos.x] == 9) continue;
+        basin.push(map[pos.y][pos.x]);
+
+        if (pos.x != 0) {
+            addPos(queue, pos.x - 1, pos.y);
+        }
+        if (pos.x != maxX - 1) {
+            addPos(queue, pos.x + 1, pos.y);
+        }
+        if (pos.y != 0) {
+            addPos(queue, pos.x, pos.y - 1);
+        }
+        if (pos.y != maxY - 1) {
+            addPos(queue, pos.x, pos.y + 1);
+        }
+    }
+
+    return basin.length;
+}
+
+function soln2(input: string) : void {
+    let map = input.split("\n").map(row => row.split("").map(val => parseInt(val)));
+
+    let lastIdx = map.length;
+    let basinSizes: number[] = [];
+    map.forEach((row, y) => {
+        row.forEach((val, x) => {
+            if (!checkVals(map, x, y, row.length, lastIdx)) return;
+            basinSizes.push(checkBasin(map, x, y, row.length, lastIdx));
+        });
+    });
+
+    let sorted = basinSizes.sort((a, b) => b - a);
+    let total = sorted[0] * sorted[1] * sorted[2];
+
+    console.log(total);
+}
+
+export const soln : SolnExport = {
+    desc: "Solution Template",
+    part1: soln1,
+    part2: soln2,
+};
